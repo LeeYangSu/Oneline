30405de11b5c597b26d549191e333b652a1005ae
LeeYangSu <shabranigudu@hanmail.net>
2012-10-28 17:42:10 +0900

    main이 포함된 파일, head도 만들어 초기화함.

--- oneline.C
+++ oneline.C
@@ -1,13 +1,52 @@
 #include "oneline.h"
 
-extern char press_key;
+CTAG *head;
+CTAG *current;
+char press_key;
 
 int main()
 {
+	char insert_key;
+
 	clrscr();
 	Outside_Box();
 	Inside_Box();
 	cursor_off();
 
-	return 0;
+	while(1)
+	{
+		press_key = getch();
+		insert_key = check_key_value(press_key);	
+		switch(insert_key)
+		{
+		case F10:
+			move_inverse();
+			break;
+		case F1:
+			print_string(9, 9, "Help", 7);
+			break;
+		case ALTL:
+			print_string(9, 9, "Load", 7);
+			break;
+		case ALTS:
+			print_string(9, 9, "Save", 7);
+			break;
+		case ALTA:
+			print_string(9, 9, "Save as", 7);
+			break;
+		case ALTX:
+			return 0;
+		}
+	}
+}
+
+void init_head(void)
+{
+	head = (CTAG *) malloc (sizeof(CTAG));
+	head = NULL;
+	head->prev = NULL;
+	head->next = NULL;
+	current = head;
+
+	return ;
 }
