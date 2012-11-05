dc9051d676a51fee4d6ef084c6c0241fe484a418
LeeYangSu <shabranigudu@hanmail.net>
2012-11-06 00:22:36 +0900

    구조체 좌표 이용, 테스트용 세이브 메뉴 구현

--- new.C
+++ new.C
@@ -1,10 +1,12 @@
 #include "oneline.h"
 
 extern char press_key;
+extern xy_point location;
 
 void new_function(void)
 {
 	char insert_key;
+	location.x = 1;
 
 	cursor_on();
 	move_cursor( 0, 2, 4);
@@ -12,27 +14,42 @@ void new_function(void)
 	while(1)
 	{
 		press_key = getch();
-		insert_key = check_key_value(press_key);	
-		switch(insert_key)
+		insert_key = check_key_value(press_key);
+
+		if(F10 == insert_key)
 		{
-		case F10:
 			move_inverse();
 			break;
-		case F1:
+		}
+		else if(F1 == insert_key)
+		{
 			print_string(9, 9, "Help", 7);
 			break;
-		case ALTL:
+		}
+		else if(ALTL == insert_key)
+		{
 			print_string(9, 9, "Load", 7);
 			break;
-		case ALTS:
+		}
+		else if(ALTS == insert_key)
+		{
 			print_string(9, 9, "Save", 7);
+			test_save_link();
 			break;
-		case ALTA:
+		}
+		else if(ALTA == insert_key)
+		{
 			print_string(9, 9, "Save as", 7);
 			break;
-		case ALTX:
-			move_inverse();
+		}
+		else if(ALTX == insert_key)
+		{
 			return ;
+		}
+		else if((96 < insert_key) && (123 > insert_key))
+		{
+			++location.x;
+			input_character(insert_key, location.x);
 		}
 	}
 }
