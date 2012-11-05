3d95739df9ff47578c3e9d13a351d3aa24d9a61d
LeeYangSu <shabranigudu@hanmail.net>
2012-11-06 00:03:58 +0900

    연결리스트 추가
    제일 뒷부분에 붙는 연결리스트 구현

--- enterkey.C
+++ enterkey.C
@@ -1,5 +1,7 @@
 #include "oneline.h"
 
+extern CTAG *head;
+extern CTAG *now_cur;
 extern char press_key;
 
 char check_key_value(char press_key)
@@ -20,3 +22,45 @@ char check_key_value(char press_key)
 
 	return press_key;
 }
+
+void input_character(char insert_key, int x)
+{
+	int Cnt = 1;
+	CTAG *fallow;
+	CTAG *current;
+	CTAG *newnode;
+
+	newnode = (CTAG *) malloc (sizeof(CTAG));
+	
+	if(NULL == head)
+	{
+		head = newnode;
+		newnode->next = NULL;
+		newnode->prev = NULL;
+		newnode->character = insert_key;
+		newnode->cNum = 1;
+		now_cur = newnode;
+		move_cursor(0, x, 4);
+		print_character(x, 4, insert_key, 7);
+	}
+	else
+	{
+        fallow = now_cur;
+        current = now_cur->next;
+		Cnt++;
+
+        if(current == NULL)
+        {
+            newnode->prev = fallow;
+            fallow->next = newnode;
+            now_cur = newnode;
+			newnode->character = insert_key;
+			newnode->cNum = Cnt;
+			move_cursor(0, x, 4);
+			print_character(x, 4, insert_key, 7);
+		}
+	}
+	return ;
+}
+
+
